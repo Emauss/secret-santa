e6a0dde516e9425fad22eed7571fad174e3e69e6
Emanuel Okrój <emanuel.okroj@flow2code.com>
2025-10-23 12:57:50 +0200

    Added needed resources to deploy

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,15 +2,26 @@ import { MyRecipient, Profile, Wishlist } from '../components';
 
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { useAuth } from '../hooks';
+import { isAdmin } from '../utils';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <div className='max-w-md mx-auto p-6 flex flex-col gap-6'>
       <h1 className='text-2xl font-bold'>Panel użytkownika 🎄</h1>
       <Profile />
       <Wishlist />
       <MyRecipient />
-      <button onClick={() => signOut(auth)} className='bg-red-500 text-white px-4 py-2 cursor-pointer rounded hover:bg-red-600 mt-4'>
+      {isAdmin(user?.uid) && (
+        <button onClick={() => navigate('/admin')} className='bg-blue-800 text-white px-4 py-2 cursor-pointer rounded hover:bg-blue-700 mt-4'>
+          Panel Admina
+        </button>
+      )}
+      <button onClick={() => signOut(auth)} className='bg-red-500 text-white px-4 py-2 cursor-pointer rounded hover:bg-red-600'>
         Wyloguj
       </button>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/secret-santa/',
+  build: {
+    outDir: 'build',
+  },
   plugins: [react(), tailwindcss()],
 });
